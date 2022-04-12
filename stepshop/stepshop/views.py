cf699bfa425827655b541f2aa449610410a4f519
alena6778 <khegayalena567@gmail.com>
2022-04-12 21:39:16 +0600

    update: lesson_02 final

--- stepshop/stepshop/views.py
+++ stepshop/stepshop/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')\
+    return render(request, 'index.html')
+
 
 def contacts(request):
     return render(request, 'contact.html')
